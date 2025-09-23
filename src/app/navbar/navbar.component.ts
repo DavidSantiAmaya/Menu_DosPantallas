c1397f23298d6264b4622344e32646d3b83e1a94
DavidSantiAmaya <160082902+DavidSantiAmaya@users.noreply.github.com>
2025-09-23 18:30:09 -0500

    codigo

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,6 +9,7 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
+
 export class NavbarComponent  implements OnInit {
 
   constructor() { }
